022cd0e9a9194671b2a81e3ebd622536a6e4473b
Nathan <nnguye95@uw.edu>
2023-11-15 19:05:32 -0800

    imported material icons into index.html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Footer() {
     return (
         <footer>
-            <div className="container">
+            <div className="container d-none d-sm-flex">
                 <p><a href="mailto:zgen88@uw.edu"><span className="material-icons">email</span> email us!</a></p>
                 <p><a href="tel:253-285-0537"><span className="material-icons">phone</span> call us!</a></p>
                 <p>&copy; iCoffee 2023</p>
