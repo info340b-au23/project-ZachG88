a6b49613b8aa7cdba8d8bd89584d2c0d93bae234
Zach <zgen88@uw.edu>
2023-11-20 17:08:09 -0800

    Imported coffee data and passed as prop

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './components/App';
+import {App} from './components/App';
+import COFFEE_DATA_IMPORT from './data/coffee_analysis.csv';
+
+
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={COFFEE_DATA_IMPORT}/>
   </React.StrictMode>
 );
 
