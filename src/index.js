29f82dd8efdce4b53117ebaa71a4f190facb8635
Zach <zgen88@uw.edu>
2023-11-22 12:34:21 -0800

    Finished all parts for deliverable 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import { App } from './components/App';
 import COFFEE_DATA_IMPORT from './data/coffee_analysis.csv';
 import './style.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdPi1jpjr-PPnH-3k0OnQYTsRZozc1McU",
+  authDomain: "icoffee-e298d.firebaseapp.com",
+  projectId: "icoffee-e298d",
+  storageBucket: "icoffee-e298d.appspot.com",
+  messagingSenderId: "508110516583",
+  appId: "1:508110516583:web:e3e7bb7280e47b0b8bbfc2",
+  measurementId: "G-RWHZ9ZS5R9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
